13a328bf747c5dcd8aade046319d8644486ce300
el-j <bluearc.in.sky@gmail.com>
2019-01-23 00:07:18 +0100

    more pin chooser design

--- src/components/uiTools/PinChooser/index.js
+++ src/components/uiTools/PinChooser/index.js
@@ -17,20 +17,20 @@ const PinChooser = (props) => {
 								{props.connectors.map((el,key)=> (
 									(el.type === 'in' || el.type === 'out' || el.type === 'unknown') ? (
 									<tr key={key} className='chooser-listelement'>
-											<td className='chooser-label'>{el.name} </td>
-											<td><input className='chooser-radio' type='radio' name={el.connector} value="in" onChange={props.handleOptionChange} checked={el.type === "in"}/></td>
-											<td><input className='chooser-radio' type='radio' name={el.connector} value="out" onChange={props.handleOptionChange} checked={el.type === "out"}/></td>
-											<td><input className='chooser-radio' type='radio' name={el.connector} value="unknown" onChange={props.handleOptionChange} checked={el.type === "unknown"}/></td>
+											<td className='chooser-label'><span>{el.name}</span> </td>
+											<td><span className='checkmark'></span><input className='chooser-radio' type='radio' name={el.connector} value='in' onChange={props.handleOptionChange} checked={el.type === 'in'}/></td>
+											<td><span className='checkmark'></span><input className='chooser-radio' type='radio' name={el.connector} value='out' onChange={props.handleOptionChange} checked={el.type === 'out'}/></td>
+											<td><span className='checkmark'></span><input className='chooser-radio' type='radio' name={el.connector} value='unknown' onChange={props.handleOptionChange} checked={el.type === 'unknown'}/></td>
 									</tr>
 									):(null)
 								))}
 						</table>
-						<button className="submitChangesButton" type="submit" onClick={props.handleFormSubmit}>
+						<button className='submitChangesButton' type='submit' onClick={props.handleFormSubmit}>
 						Save
 						</button>
 					</div>
 					<div>
-					<div className="chooser-svg"  dangerouslySetInnerHTML={{__html: props.svg}} />
+					<div className='chooser-svg'  dangerouslySetInnerHTML={{__html: props.svg}} />
 					</div>
 				</div>
 			</div>
