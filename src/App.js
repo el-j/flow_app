c0702312bf05efda0b346ddcea7f10eedff3493f
el-j <bluearc.in.sky@gmail.com>
2019-01-23 20:41:53 +0100

    remove stuff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
-import DemoOne from './demos/DemoOne';
-import DemoTwo from './demos/DemoTwo';
-import DemoThree from './demos/DemoThree';
-import DemoFour from './demos/DemoFour';
-import DemoFive from './demos/DemoFive';
-import DemoSix from './demos/DemoSix';
-import DemoSeven from './demos/DemoSeven';
+
 import Flow from './Flow';
 import Menu from './Menu';
 import './App.css';
@@ -17,13 +11,6 @@ class App extends Component {
       <HashRouter>
         <div className="app">
           <Switch>
-            <Route path="/demo1" component={DemoOne} />
-            <Route path="/demo2" component={DemoTwo} />
-            <Route path="/demo3" component={DemoThree} />
-            <Route path="/demo4" component={DemoFour} />
-            <Route path="/demo5" component={DemoFive} />
-            <Route path="/demo6" component={DemoSix} />
-            <Route path="/demo7" component={DemoSeven} />
             <Route path="/flow" component={Flow} />
             <Route component={Menu} />
           </Switch>
--- src/Menu.jsx
+++ src/Menu.jsx
@@ -5,38 +5,16 @@ class Menu extends React.Component {
 	render() {
 		return (
 			<div>
-				<h1>Live Demos of Storm React Diagrams</h1>
+				<h1>Fritzing Flow Example App</h1>
 				<p>
-					The original library can be found <a href="https://github.com/projectstorm/react-diagrams">here</a>.
+					The Repository can be found <a href="https://github.com/el-j/flow_app">here</a>.
 				</p>
 				<ul>
-					<li>
-						<Link to="demo1">Demo 1</Link> - Default nodes
-					</li>
-					<li>
-						<Link to="demo2">Demo 2</Link> - Check performance
-					</li>
-					<li>
-						<Link to="demo3">Demo 3</Link> - User defined nodes
-					</li>
-					<li>
-						<Link to="demo4">Demo 4</Link> - Read only diagram
-					</li>
-					<li>
-						<Link to="demo5">Demo 5</Link> - Drag and drop
-					</li>
-					<li>
-						<Link to="demo6">Demo 6</Link> - Serialise & Deserialise
-					</li>
-					<li>
-						<Link to="demo7">Demo 7</Link> - Snap to grid
-					</li>
+
 					<li>
 						<Link to="Flow">Flow</Link> - Flow Example App
 					</li>
-					<li>
-						<Link to="proEnv">production dev</Link> - production env
-					</li>
+
 				</ul>
 			</div>
 		);
